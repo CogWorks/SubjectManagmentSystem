fe9da6c626119f290f1a81b25fae54e3ee47b94d
gartea <alexdgarten@gmail.com>
2017-09-18 13:37:14 -0400

    added gui support and gui class

--- participantid.py
+++ participantid.py
@@ -1,4 +1,6 @@
 import sqlite3
+import participant_gui
+from tkinter import Tk
 
 #takes a rin/id tuple and connection ubject
 def add_to_database(rin_id, conn):
@@ -10,20 +12,25 @@ def add_to_database(rin_id, conn):
     c.execute("INSERT INTO rin__testing_id VALUES (?, ?)", (rin_id[0], rin_id[1]))
     conn.commit()
 
+def connect_to_database(db):
+    conn = sqlite3.connect(db)
+    c = conn.cursor()
+    try:
+        c.execute('CREATE TABLE rin__testing_id (rin text unique, testing_id text unique)')
+    except sqlite3.OperationalError as e:
+        pass
+    return conn
 
-conn = sqlite3.connect('test.db')
+#gui startup
+app = participant_gui.App_Gui()
+app.mainloop()
 
-c = conn.cursor()
-
-try:
-    c.execute('CREATE TABLE rin__testing_id (rin text unique, testing_id text unique)')
-except sqlite3.OperationalError as e:
-    pass
-
-add_to_database(('a','b'),conn)
 
 #testing
-c.execute('SELECT * FROM rin__testing_id')
-print (c.fetchall())
-
-conn.close()
+# conn = connect_to_database('test.db')
+# c = conn.cursor()
+#
+# c.execute('SELECT * FROM rin__testing_id')
+# print (c.fetchall())
+#
+# conn.close()
